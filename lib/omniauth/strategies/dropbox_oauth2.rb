b439d7fa561db162560a2b93218e8623531b60d8
ronniekessler <rlkessler@gmail.com>
2018-03-29 11:57:42 +1100

    Change Dropbox api url to new v2 url.

--- lib/omniauth/strategies/dropbox_oauth2.rb
+++ lib/omniauth/strategies/dropbox_oauth2.rb
@@ -5,9 +5,9 @@ module OmniAuth
     class DropboxOauth2 < OmniAuth::Strategies::OAuth2
       option :name, "dropbox_oauth2"
       option :client_options, {
-        :site               => 'https://api.dropbox.com',
+        :site               => 'https://api.dropboxapi.com',
         :authorize_url      => 'https://www.dropbox.com/oauth2/authorize',
-        :token_url          => 'https://api.dropbox.com/oauth2/token'
+        :token_url          => 'https://api.dropboxapi.com/oauth2/token'
       }
 
       uid { raw_info['uid'] }
@@ -25,7 +25,7 @@ module OmniAuth
       end
 
       def raw_info
-        conn = Faraday.new(:url => 'https://api.dropbox.com') do |faraday|
+        conn = Faraday.new(:url => 'https://api.dropboxapi.com') do |faraday|
           faraday.request  :url_encoded             # form-encode POST params
           faraday.response :logger                  # log requests to STDOUT
           faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
